cc925e1741499ee8dc4d7db79de61a405c464f5f
Anxiaolu <1466022881@qq.com>
2017-06-06 21:06:54 +0800

    注释添加

--- SpringLearning1/src/main/java/cn/edu/sdut/softlab/springlearning1/UseFunctionService.java
+++ SpringLearning1/src/main/java/cn/edu/sdut/softlab/springlearning1/UseFunctionService.java
@@ -9,16 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
+ * 使用@Service注解声明当前UseFunctionService类是Spring管理的一个Bean.
+ * 使用@autowired将Function的实体Bean注入到UseFunctionService中,
+ * 让UseFunctionService具备FunctionService的功能
+ * ,此处使用JSR-330@inject注解或者使用JSR-250注解的@Resource注解是等效的.
  *
  * @author huanlu
  */
 @Service
-//使用@Service注解声明当前UseFunctionService类是Spring管理的一个Bean
 public class UseFunctionService {
+
     @Autowired
     FunctionService functionService;
-    
-    public String SayHello(String word){
+
+    public String SayHello(String word) {
         return functionService.sayHello(word);
     }
 }
