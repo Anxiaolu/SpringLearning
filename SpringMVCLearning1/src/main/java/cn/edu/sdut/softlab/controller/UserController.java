374251cdc8be57a66495580f3b39e3cf91544a11
Anxiaolu <1466022881@qq.com>
2017-08-19 18:25:05 +0800

    修改

---
+++ SpringMVCLearning1/src/main/java/cn/edu/sdut/softlab/controller/UserController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.edu.sdut.softlab.controller;
+
+/**
+ *
+ * @author huanlu
+ */
+public class UserController {
+    
+}
